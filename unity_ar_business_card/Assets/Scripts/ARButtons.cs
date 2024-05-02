d7015afb805c6fd5c9ea69a725d6229d5ba44d46
Jagger Van Winkle <JaggerVanW@gmail.com>
2024-05-02 01:06:29 -0500

    Task 1 & 3 (Need to work on 2)

---
+++ unity_ar_business_card/Assets/Scripts/ARButtons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARButtons : MonoBehaviour
+{
+    public void GitHubURL()
+    {
+        Application.OpenURL("https://github.com/JiggyJoggy");
+    }
+
+    public void LinkedInURL()
+    {
+        Application.OpenURL("https://linkedin.com/in/jaggervw");
+    }
+
+    public void GmailURL()
+    {
+        Application.OpenURL("https://JaggerVanW@gmail.com");
+    }
+
+    public void TwitchURL()
+    {
+        Application.OpenURL("https://twitch.tv/jaggpegg_");
+    }
+}
